1223adc0dd26fc58647933115bb2f9f733e5e603
Aleksandar Vuckovic <aleksandar.23v@gmail.com>
2020-10-19 12:56:30 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import AccountsTable from './components/AccountsTable/AccountsTable';
+import AddAccount from './components/AddAccount/AddAccount';
+import EditAccount from './components/EditAccount/EditAccount';
+import EditTable from './components/EditTable/EditTable';
+import Header from './components/Header/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state ={
+        accounts: [
+            {id:1, name:'Aleksandar', lastname: 'Vuckovic', phone: '11-11-11', email:'aca@gmail.com'},
+            {id:2, name:'Nikola', lastname: 'Mrkonja', phone: '22-22-22', email:'dzoni@gmail.com'}
+        ]
+    }
+
+    addNewAccountToState = (acc) => {
+        this.setState({
+            accounts: [...this.state.accounts, acc]
+        })
+    }
+
+    deleteAccount = (id) => {
+        const accountsCopy = [...this.state.accounts];
+        const newCopyAccounts = accountsCopy.filter(account => {
+            return account.id !== id
+        })
+        this.setState({accounts: newCopyAccounts})
+    }
+
+    editAccount = (acc) => {
+        const copyAccounts = [...this.state.accounts];
+        let accountPossition = copyAccounts.map(account => account.id).indexOf(acc.id)
+        copyAccounts[accountPossition] = acc;
+        this.setState({accounts: copyAccounts})
+    }
+
+    render() {
+        return (
+            <BrowserRouter>
+                <Header />
+                <Route path='/' exact>
+                    <AccountsTable accounts={this.state.accounts}/>
+                </Route>
+                <Route path = '/add'>
+                    <AddAccount addNewAccountToState={this.addNewAccountToState}/>
+                </Route>
+                <Switch>
+                <Route path='/edit/:id' >
+                    <EditAccount accounts={this.state.accounts} editAccount={this.editAccount} />
+                </Route>
+                <Route path='/edit'>
+                    <EditTable  accounts={this.state.accounts} deleteAccount={this.deleteAccount} />
+                </Route>  
+                </Switch>        
+            </BrowserRouter>
+
+        )
+    }
 }
 
 export default App;
---
+++ src/components/AddAccount/AddAccount.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+class AddAccount extends Component {
+
+    state ={
+        newAccount : {
+            id: '',
+            name: '',
+            lastname: '',
+            phone: '',
+            email: ''
+        }
+    }
+
+    changeHandler = (e) => {
+        let change = e.target.id;
+        let newAccountCopy = {...this.state.newAccount}
+        newAccountCopy[change] = e.target.value;
+        this.setState({newAccount: newAccountCopy});
+        
+    }
+
+    addNewAccount = () => {
+        this.props.addNewAccountToState(this.state.newAccount)
+        this.props.history.push('/')
+    }
+
+    
+    render(){
+        return(
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 offset-1">
+                        <h2 className='display-4 m-4'>Add Account</h2>
+                        <div className="row">
+                            <div className="col-10 offset-1">
+                                <input type="text" onChange={this.changeHandler} id='id' placeholder='id' className='form-control'/><br/>
+                                <input type="text" onChange={this.changeHandler} id='name' placeholder='name' className='form-control'/><br/>
+                                <input type="text" onChange={this.changeHandler} id='lastname' placeholder='lastname' className='form-control'/><br/>
+                                <input type="text" onChange={this.changeHandler} id='phone' placeholder='phone' className='form-control'/><br/>
+                                <input type="text" onChange={this.changeHandler} id='email' placeholder='email' className='form-control'/><br/>
+                                <button onClick={this.addNewAccount} className= 'btn btn-primary form-control'>Save</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default withRouter(AddAccount);
